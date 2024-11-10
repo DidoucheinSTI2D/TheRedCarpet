0fefa5c61791375c0f2426513b32200f4c5ba6e5
DidoucheInSTI2D <didouche78130@gmail.com>
2024-11-11 00:03:29 +0100

    fix setter/getter

--- src/entity/User.php
+++ src/entity/User.php
@@ -14,6 +14,23 @@ class User {
         $this->pdo = $pdo;
     }
 
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+    
     public function save(\PDO $pdo): void {
         $sql = 'INSERT INTO USER (username, password) VALUES (:username, :password)';
         $stmt = $pdo->prepare($sql);
