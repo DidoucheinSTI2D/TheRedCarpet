330c7f2bff3a0572743409426851ab3be715c8d8
Mikael Lahlou <lahmikael@gmail.com>
2024-11-22 13:44:20 +0100

    Login function

--- frontend/TheRedCarpet/src/api/API.js
+++ frontend/TheRedCarpet/src/api/API.js
@@ -2,7 +2,7 @@ export const getRegister = async (username, password) => {
   try {
     const apiHost = import.meta.env.VITE_API_HOST || "http://localhost:8888";
     const response = await fetch(
-      `${apiHost}/TheRedCarpet/backend/API/register.php`,
+      `${apiHost}/API/user/register.php`,
       {
         method: "POST",
         headers: {
@@ -21,6 +21,39 @@ export const getRegister = async (username, password) => {
       return { message: data.message, error: false };
     } else {
       return { message: data.message || "An error occurred.", error: true };
+    }
+  } catch (err) {
+    return {
+      message: "Failed to connect to the server. " + err.message,
+      error: true,
+    };
+  }
+};
+
+
+export const getLogin = async (username, password) => {
+  try {
+    const apiHost = import.meta.env.VITE_API_HOST || "http://localhost:8888";
+    const response = await fetch(
+      `${apiHost}/API/user/login.php`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      }
+    );
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return { message: data.message, error: false, user: data.user };
+    } else {
+      return { message: data.message || "Login failed.", error: true };
     }
   } catch (err) {
     return {
