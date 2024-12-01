75006b3ec9c10d0474bc2468e9b2eeded75deb58
Mikael Lahlou <lahmikael@gmail.com>
2024-12-01 18:07:04 +0100

    vite config

--- frontend/TheRedCarpet/vite.config.js
+++ frontend/TheRedCarpet/vite.config.js
@@ -7,10 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@api": path.resolve(
-        path.dirname(new URL(import.meta.url).pathname),
-        "src/api"
-      ),
+      "@api": path.resolve(__dirname, "src/api"),
     },
   },
 });
