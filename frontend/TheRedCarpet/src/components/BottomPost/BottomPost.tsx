35cf9e044ce24132cc1e16d2d23c88abb17a6a93
Louis <louisdondey@gmail.com>
2024-12-18 20:15:34 +0100

    Better design for homepage

--- frontend/TheRedCarpet/src/components/BottomPost/BottomPost.tsx
+++ frontend/TheRedCarpet/src/components/BottomPost/BottomPost.tsx
@@ -21,31 +21,32 @@ const Test = ({likes, description}) => {
 
 
     return (
-        <div className="flex flex-col gap-[1rem] justify-between"> 
-            <div className='flex'>
+        <div className="flex flex-col justify-between gap-[2rem]"> 
+            <div className='flex flex-col gap-[1rem]'>
                 <div className='flex gap-[1rem]'>
-                    <button className='flex justify-center items-center size-[3rem] border-2 rounded-full border-black'><img src={Like} alt="like" className=' size-[3rem]'/></button>
-                    <button className='flex justify-center items-center size-[3rem] border-2 rounded-full border-black'><img src={Comment} alt="comment" className=' size-[2rem] '/></button>
+                    <button className='flex justify-center items-center size-[3rem] border-2 rounded-full border-white'><img src={Like} alt="like" className=' size-[3rem]'/></button>
+                    <button className='flex justify-center items-center size-[3rem] border-2 rounded-full border-white'><img src={Comment} alt="comment" className=' size-[2rem] '/></button>
+                </div>
+                <div className='flex'>
+                    {likes} {(likes>1)?"Likes":"Like"}
+                </div>
+                <div className='flex flex-wrap items-center justify-start gap-[.1rem]'>
+                    <p className="transition-all duration-300">
+                    {displayedText}
+                    {!isExpanded && isTruncatable && '...'}
+                    </p>
+                    {isTruncatable && (
+                        <button
+                        onClick={toggleExpand}
+                        className="text-black-500 underline"
+                        >
+                        {isExpanded ? 'See less' : 'See more'}
+                        </button>
+                    )}
                 </div>
             </div>
-            <div className='flex'>
-                {likes} {(likes>1)?"Likes":"Like"}
-            </div>
-            <div className='flex items-center justify-start  gap-[1rem]'>
-                <p className="transition-all duration-300">
-                {displayedText}
-                {!isExpanded && isTruncatable && '...'}
-                </p>
-                {isTruncatable && (
-                    <button
-                    onClick={toggleExpand}
-                    className="text-black-500 underline"
-                    >
-                    {isExpanded ? 'See less' : 'See more'}
-                    </button>
-                )}
-            </div>
-            <button className='flex hover:underline'>See comments</button>
+            
+            <button className='flex hover:underline w-fit'>See comments</button>
         </div>
     );
 };
