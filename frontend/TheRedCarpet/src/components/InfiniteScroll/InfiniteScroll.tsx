35723c83c71874c8a2dac652d5c19eddbe3f48ce
Louis <louisdondey@gmail.com>
2024-12-18 20:17:36 +0100

    Better design for homepage

--- frontend/TheRedCarpet/src/components/InfiniteScroll/InfiniteScroll.tsx
+++ frontend/TheRedCarpet/src/components/InfiniteScroll/InfiniteScroll.tsx
@@ -24,63 +24,79 @@ function InfiniteScroll() {
     threshold: 0.5,
   });
 
-  const spectacles = [
-    {
-      "spectacle_id": 1,
-      "spectacle_title": "Le Roi Lion",
-      "spectacle_synopsis": "Une adaptation musicale du film classique de Disney, racontant l'histoire de Simba, un jeune lion qui doit retrouver son chemin après la perte de son père."
-    },
-    {
-      "spectacle_id": 2,
-      "spectacle_title": "Les Misérables",
-      "spectacle_synopsis": "Une épopée musicale basée sur le roman de Victor Hugo, suivant la vie de Jean Valjean et les luttes pour la justice et la rédemption dans la France du XIXe siècle."
-    },
-    {
-      "spectacle_id": 3,
-      "spectacle_title": "Carmen",
-      "spectacle_synopsis": "Un opéra passionné de Georges Bizet, qui raconte l'histoire tragique d'amour entre Don José et la belle gitane Carmen."
-    },
-    {
-      "spectacle_id": 4,
-      "spectacle_title": "Notre-Dame de Paris",
-      "spectacle_synopsis": "Une comédie musicale inspirée du roman de Victor Hugo, mettant en scène l'amour impossible entre Quasimodo, le bossu de Notre-Dame, et la belle Esmeralda."
-    },
-    {
-      "spectacle_id": 5,
-      "spectacle_title": "Hamilton",
-      "spectacle_synopsis": "Une comédie musicale révolutionnaire qui retrace la vie d'Alexander Hamilton, l'un des pères fondateurs des États-Unis, à travers le hip-hop et le R&B."
-    },
-    {
-      "spectacle_id": 6,
-      "spectacle_title": "Mamma Mia!",
-      "spectacle_synopsis": "Une comédie musicale joyeuse basée sur les chansons du groupe ABBA, racontant l'histoire d'une jeune femme qui cherche à découvrir l'identité de son père avant son mariage."
-    },
-    {
-      "spectacle_id": 7,
-      "spectacle_title": "Phantom of the Opera",
-      "spectacle_synopsis": "Un opéra musical de Andrew Lloyd Webber, qui suit l'histoire d'un mystérieux fantôme qui hante l'Opéra de Paris et son obsession pour la chanteuse Christine Daaé."
-    },
-    {
-      "spectacle_id": 8,
-      "spectacle_title": "West Side Story",
-      "spectacle_synopsis": "Une adaptation moderne de Roméo et Juliette, se déroulant à New York, où deux gangs rivaux s'affrontent, tandis que deux jeunes amoureux tentent de surmonter les obstacles de leur environnement."
-    },
-    {
-      "spectacle_id": 9,
-      "spectacle_title": "Billy Elliot",
-      "spectacle_synopsis": "Une comédie musicale émouvante sur un jeune garçon qui découvre sa passion pour la danse, défiant les attentes de sa famille et de sa communauté dans une ville minière du nord de l'Angleterre."
-    },
-    {
-      "spectacle_id": 10,
-      "spectacle_title": "Les producteurs",
-      "spectacle_synopsis": "Une comédie musicale hilarante sur un producteur de Broadway qui tente de monter le pire spectacle jamais réalisé pour escroquer des investisseurs."
-    },
-  ];
+  // const spectacles = [
+  //   {
+  //     "spectacle_id": 1,
+  //     "spectacle_title": "Le Roi Lion",
+  //     "spectacle_synopsis": "Une adaptation musicale du film classique de Disney, racontant l'histoire de Simba, un jeune lion qui doit retrouver son chemin après la perte de son père."
+  //   },
+  //   {
+  //     "spectacle_id": 2,
+  //     "spectacle_title": "Les Misérables",
+  //     "spectacle_synopsis": "Une épopée musicale basée sur le roman de Victor Hugo, suivant la vie de Jean Valjean et les luttes pour la justice et la rédemption dans la France du XIXe siècle."
+  //   },
+  //   {
+  //     "spectacle_id": 3,
+  //     "spectacle_title": "Carmen",
+  //     "spectacle_synopsis": "Un opéra passionné de Georges Bizet, qui raconte l'histoire tragique d'amour entre Don José et la belle gitane Carmen."
+  //   },
+  //   {
+  //     "spectacle_id": 4,
+  //     "spectacle_title": "Notre-Dame de Paris",
+  //     "spectacle_synopsis": "Une comédie musicale inspirée du roman de Victor Hugo, mettant en scène l'amour impossible entre Quasimodo, le bossu de Notre-Dame, et la belle Esmeralda."
+  //   },
+  //   {
+  //     "spectacle_id": 5,
+  //     "spectacle_title": "Hamilton",
+  //     "spectacle_synopsis": "Une comédie musicale révolutionnaire qui retrace la vie d'Alexander Hamilton, l'un des pères fondateurs des États-Unis, à travers le hip-hop et le R&B."
+  //   },
+  //   {
+  //     "spectacle_id": 6,
+  //     "spectacle_title": "Mamma Mia!",
+  //     "spectacle_synopsis": "Une comédie musicale joyeuse basée sur les chansons du groupe ABBA, racontant l'histoire d'une jeune femme qui cherche à découvrir l'identité de son père avant son mariage."
+  //   },
+  //   {
+  //     "spectacle_id": 7,
+  //     "spectacle_title": "Phantom of the Opera",
+  //     "spectacle_synopsis": "Un opéra musical de Andrew Lloyd Webber, qui suit l'histoire d'un mystérieux fantôme qui hante l'Opéra de Paris et son obsession pour la chanteuse Christine Daaé."
+  //   },
+  //   {
+  //     "spectacle_id": 8,
+  //     "spectacle_title": "West Side Story",
+  //     "spectacle_synopsis": "Une adaptation moderne de Roméo et Juliette, se déroulant à New York, où deux gangs rivaux s'affrontent, tandis que deux jeunes amoureux tentent de surmonter les obstacles de leur environnement."
+  //   },
+  //   {
+  //     "spectacle_id": 9,
+  //     "spectacle_title": "Billy Elliot",
+  //     "spectacle_synopsis": "Une comédie musicale émouvante sur un jeune garçon qui découvre sa passion pour la danse, défiant les attentes de sa famille et de sa communauté dans une ville minière du nord de l'Angleterre."
+  //   },
+  //   {
+  //     "spectacle_id": 10,
+  //     "spectacle_title": "Les producteurs",
+  //     "spectacle_synopsis": "Une comédie musicale hilarante sur un producteur de Broadway qui tente de monter le pire spectacle jamais réalisé pour escroquer des investisseurs."
+  //   },
+  // ];
+
+  // useEffect(() => {
+  //   setRepresentations(spectacles);
+  //   setVisibleRepresentations(spectacles.slice(0, 5));
+  //   setIsLoading(false);
+  // }, []);
 
   useEffect(() => {
-    setRepresentations(spectacles);
-    setVisibleRepresentations(spectacles.slice(0, 5));
-    setIsLoading(false);
+    const fetchData = async () => {
+      const result = await getOngoingRepresentations();
+      if (!result.error) {
+        setRepresentations(result.data);
+        setVisibleRepresentations(result.data.slice(0, 5));
+        setIsLoading(false);
+      } else {
+        console.error("Error fetching representations:", result.message);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
   useEffect(() => {
@@ -101,7 +117,7 @@ function InfiniteScroll() {
 
   return (
     <div className="flex flex-col items-center justify-center p-4 gap-[4rem]">
-      <div className="flex flex-col items-center justify-center h-[50vh] w-full gap-[1rem]">
+      <div className="flex flex-col items-center justify-center h-[50vh] w-full">
         <p className=" text-3xl font-bold">Magic pick</p>
         <p className=" text-xl font-bold">Your Ultimate Guide to Theatre and Cinema Experiences!</p>
       </div>
